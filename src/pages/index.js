02831412ccfa08eb26e324f98079dc3533f0f4f4
Murphy Hsieh <murphyhsieh@leandev.io>
2020-02-05 13:41:03 +0800

    add the gatsby-plugin-mdx plugin, and creating the first mdx file

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,11 @@
 import React from "react";
-import Layout from "../components/layout"
+import Layout from "../components/layout";
+import { Link } from 'gatsby';
 
-export default () => <Layout>Hello world!</Layout>
+export default () =>
+  <Layout>
+    Hello world!
+    <div>
+      <Link to="/post">POST MDX</Link>
+    </div>
+  </Layout>
