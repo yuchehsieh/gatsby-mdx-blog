fe835d2caeabc52010a78cbf8a3cbc899b056e07
Murphy Hsieh <murphyhsieh@leandev.io>
2020-02-06 14:46:42 +0800

    add the .mdx's inline-image, included the dependency using different way, remove the Counter in .mdx, 直接在 .mdx 加 className 不能用 module.css

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,5 +17,37 @@ module.exports = {
         path: `${__dirname}/src/posts`,
       },
     },
+
+    /**
+     * the following provided by Udemy's students
+     */
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        gatsbyRemarkPlugins: [ `gatsby-remark-images` ],
+        plugins: [ `gatsby-remark-images` ],
+      }
+    },
+
+    /**
+     * the following provided by Gatsby.org
+     */
+    // `gatsby-plugin-sharp`,
+    // {
+    //   resolve: `gatsby-transformer-remark`,
+    //   options: {
+    //     plugins: [
+    //       {
+    //         resolve: `gatsby-remark-images`,
+    //         options: {
+    //           // It's important to specify the maxWidth (in pixels) of
+    //           // the content container as this plugin uses this as the
+    //           // base for generating different widths of each image.
+    //           maxWidth: 590,
+    //         },
+    //       },
+    //     ],
+    //   },
+    // },
   ]
 }
