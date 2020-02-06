0154150a4d32d62d90ca0385b91918e9105df700
Murphy Hsieh <murphyhsieh@leandev.io>
2020-02-06 14:07:23 +0800

    add the template's single-post-query, and render the markdown through MDXRenderer, also could directly change the style using template's css rule (h1, h2, p)

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -3,15 +3,55 @@ import styles from '../css/postTemplate.module.css';
 import { Link, graphql } from 'gatsby';
 import Image from 'gatsby-image';
 import { MDXRenderer } from 'gatsby-plugin-mdx';
+import Layout from "../components/layout"
 
-const PostTemplate = ({pageContext}) => {
-  console.log(pageContext);
+const PostTemplate = ({data}) => {
+
+  const { body } = data.mdx;
+  const { author, date, title, image}  = data.mdx.frontmatter;
+  const mainImg = image.childImageSharp.fluid;
 
   return (
-    <div>
-      this is the post template!
-    </div>
+    <Layout>
+      <section className={styles.template}>
+        <Link to={"/"} className={styles.link}>back to all posts</Link>
+        <div className={styles.info}>
+          <h1>{title}</h1>
+          <h4>
+            <span>by {author}</span> / <span>{date}</span>
+          </h4>
+        </div>
+        <div className={styles.postImage}>
+          <Image fluid={mainImg}/>
+        </div>
+        <div className={styles.content}>
+          <MDXRenderer>
+            {body}
+          </MDXRenderer>
+        </div>
+      </section>
+    </Layout>
   )
 }
+
+export const getPost = graphql`
+  query ($slug: String!) {
+    mdx(frontmatter: {slug: {eq: $slug}}) {
+      body
+      frontmatter {
+        author
+        date(formatString: "MMMM Do, YYYY")
+        title
+        image {
+          childImageSharp {
+            fluid {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default PostTemplate
